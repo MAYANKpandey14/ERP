6bf4422b5f35ae67a3bb9db34f6f8f58ad8ccc21
MAYANKpandey14 <mayankpandeyofficial404@gmail.com>
2023-06-04 12:27:04 +0530

    Added auth functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+let user1 ={
+    name:"Mayank Pandey",
+    PNO:"141020011",
+    password:"Happyme@123"
+};
+
+let pno = document.querySelector('#uid');
+let password= document.querySelector('#pass');
+let button = document.querySelector('#btn');
+
+
+//  function checkDetail(){
+//     document.querySelector('#btn').setAttribute("disabled");
+//     if(pno===user1.PNO && password==user1.password){
+       
+//         document.querySelector('#btn').removeAttribute("disabled");
+//     }
+//     else{
+//         document.querySelector('.alert').innerHTML+= "Wrong PNO or Password.";
+//     }
+// }
+
+
+
+
